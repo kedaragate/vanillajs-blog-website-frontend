40680ee973b83f4e3f071b888c28276501441ece
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-10-28 22:32:55 +0530

    update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ container.insertAdjacentHTML("afterbegin",result.join(","))
   .catch((err) => console.log(err));
 
 const createHTML = function (item) {
-  return`<h3>${item.title}</h3>
-    <h3>${item.author}</h3>
-    <p>${item.body}</p>`;
+  return`<h3 class="title" name="title" id=${item.id}>${item.title}</h3>
+    <h3 class="author" name="author">${item.author}</h3>
+    <p class="body" name="body">${item.body}</p>`;
     
 };
