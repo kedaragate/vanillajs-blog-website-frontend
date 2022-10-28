742ae30d76a3019dd2b7370fa32d49a2f1f42e4c
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-10-28 17:34:00 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ const blogTitle = document.querySelector(".blog-title");
 
 const blogDescription = document.querySelector(".blog-description");
 const body = document.getElementsByTagName("body");
-const container=document.querySelector(".container")
-console.log(container)
+const container=document.getElementById("container")
+
 
 fetch("https://node-js-app-with-auth.herokuapp.com/api/blogs")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
-    console.log(container)
-    data.forEach((element) => {
-      return createHTML(element);
-      
+     const result=
+     data.map(ele=>createHTML(ele))
+     container.insertAdjacentHTML
      
-    });
+     ("afterbegin",result.join(","))
+    
   })
   .catch((err) => console.log(err));
 
 const createHTML = function (item) {
-  const e=`<h3>${item.title}</h3>
+  return`<h3>${item.title}</h3>
     <h3>${item.author}</h3>
     <p>${item.body}</p>`;
-    return container.insertAdjacentHTML("beforebegin",e)
+    
 };
